aac02b46f6c9f328c57dab2765a2939047caf515
HitBlast <hitblastlive@gmail.com>
2025-03-09 16:14:13 +0600

    updated cutler formulae

--- Formula/cutler.rb
+++ Formula/cutler.rb
@@ -1,8 +1,8 @@
 class Cutler < Formula
   desc "Declarative macOS settings management at your fingertips, with speed."
   homepage "https://github.com/hitblast/cutler"
-  url "https://github.com/hitblast/cutler/releases/download/v0.1.3/cutler-v0.1.3-darwin-arm64.zip"
-  sha256 "844fcd36a2baacff4240b7de0f059f565d25ed4f02e4f8cf483ecb0ceb81711c"
+  url "https://github.com/hitblast/cutler/releases/download/v0.1.4/cutler-v0.1.4-darwin-arm64.zip"
+  sha256 "763ec06e58bc9dd27131ae72fc9685171722d5aa7b458032345d2aad5cc4d8ef"
   license "MIT"
 
   depends_on macos: :sonoma # macOS 14

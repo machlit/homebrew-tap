98df2911f9d2369c6fefac78bd12e16c81d258e4
HitBlast <hitblastlive@gmail.com>
2025-03-09 10:36:06 +0600

    update formula

--- Formula/cutler.rb
+++ Formula/cutler.rb
@@ -1,5 +1,8 @@
 class Cutler < Formula
-  version 'v0.1.2'
+  def version
+    "v0.1.2"
+  end
+
   sha256 "6d2e2ec9c4603920be5dc518f33478999551a98892f850924d16a739fc8c6b3f"
 
   url "https://github.com/hitblast/cutler/releases/download/#{version}/cutler-#{version}-darwin-arm64.zip"

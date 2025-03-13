c441161483998813c01240e49f7f39be04c8836d
HitBlast <hitblastlive@gmail.com>
2025-03-13 12:43:48 +0600

    updated version

--- Casks/alter.rb
+++ Casks/alter.rb
@@ -1,8 +1,8 @@
 cask "alter" do
-  version '1.0.0'
-  sha256 "cc97596c970129064d1c8af3ce296142669328dafe9540067dd0514d43a60e37"
+  version '1.0.0-fix'
+  sha256 "52379bedf5aec6cce2894280d3b8c98d26b1f8d4d475a08330dd677a12f9c8c1"
 
-  url "https://github.com/hitblast/Alter/releases/download/v#{version}/alter-v#{version}-darwin-arm64.zip"
+  url "https://github.com/hitblast/Alter/releases/download/v1.0.0/alter-v1.0.0-darwin-arm64.zip"
   name "Alter"
   desc "Alter is a minimal macOS app which can be used to customize your app icons with ease."
   homepage "https://github.com/hitblast/Alter"

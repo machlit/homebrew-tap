c56003fc206cff0cdc80c79d89a07a8364d2236d
HitBlast <hitblastlive@gmail.com>
2025-03-13 12:29:52 +0600

    updated dependency requirements

--- Casks/alter.rb
+++ Casks/alter.rb
@@ -7,7 +7,7 @@ cask "alter" do
   desc "Alter is a minimal macOS app which can be used to customize your app icons with ease."
   homepage "https://github.com/hitblast/Alter"
 
-  depends_on macos: ">= :sonoma" # macOS 14
+  depends_on macos: ">= :ventura" # macOS 13
 
   postflight do
     system "xattr -d com.apple.quarantine #{appdir}/Alter.app"

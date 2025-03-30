5cfb853f6056062d83c55f5cb726e3faeff9609d
HitBlast <hitblastlive@gmail.com>
2025-03-30 14:50:05 +0600

    updated alter

--- Casks/alter.rb
+++ Casks/alter.rb
@@ -1,6 +1,6 @@
 cask "alter" do
-  version '1.1.0'
-  sha256 "8de035a9923714e13dfab4c5cb9c796f0b01af2b19f672b5fd0b3634a3dc56fa"
+  version '1.1.1'
+  sha256 "940c7e29932a822a80c4aeaefae3230faa4b4b2f1c95b71873cd0f9db385ce06"
 
   url "https://github.com/hitblast/Alter/releases/download/v#{version}/alter-v#{version}-darwin-arm64.zip"
   name "Alter"

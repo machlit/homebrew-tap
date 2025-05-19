8f200a2f1d7128238966c5f143174a3cf2fce1c5
HitBlast <hitblastlive@gmail.com>
2025-05-19 16:52:41 +0600

    updated casks + formula

--- Casks/alter.rb
+++ Casks/alter.rb
@@ -1,17 +1,19 @@
-cask "alter" do
+# frozen_string_literal: true
+
+cask 'alter' do
   version '1.1.4'
-  sha256 "4cd70f5babff08e5368d77d3cbb0c72031eb1f796a4e52c18e5fef92fb515c05"
+  sha256 '4cd70f5babff08e5368d77d3cbb0c72031eb1f796a4e52c18e5fef92fb515c05'
 
   url "https://github.com/hitblast/Alter/releases/download/v#{version}/alter-v#{version}-darwin-arm64.zip"
-  name "Alter"
-  desc "Alter is a minimal macOS app which can be used to customize your app icons with ease."
-  homepage "https://github.com/hitblast/Alter"
+  name 'Alter'
+  desc 'Alter is a minimal macOS app which can be used to customize your app icons with ease.'
+  homepage 'https://github.com/hitblast/Alter'
 
-  depends_on macos: ">= :ventura" # macOS 13
+  depends_on macos: '>= :ventura' # macOS 13
 
   postflight do
-    system "xattr -d com.apple.quarantine #{appdir}/Alter.app"
+    system 'xattr -d com.apple.quarantine #{appdir}/Alter.app'
   end
 
-  app "Alter.app"
+  app 'Alter.app'
 end
--- Formula/cutler.rb
+++ Formula/cutler.rb
@@ -1,16 +1,19 @@
+# frozen_string_literal: true
+
+# Homebrew formula for Cutler: Powerful, declarative settings management for your Mac, with speed.
 class Cutler < Formula
-  version "0.6.3"
-  desc "Powerful, declarative settings management for your Mac, with speed."
-  homepage "https://github.com/hitblast/cutler"
+  version '0.6.3'
+  desc 'Powerful, declarative settings management for your Mac, with speed.'
+  homepage 'https://github.com/hitblast/cutler'
   url "https://github.com/hitblast/cutler/releases/download/v#{version}/cutler-v#{version}-darwin-arm64.zip"
-  sha256 "1e0a3c4d171d62c003103b766bfd8e2f698975899fb78e4c06f88f075c02cbae"
-  license "MIT"
+  sha256 '1e0a3c4d171d62c003103b766bfd8e2f698975899fb78e4c06f88f075c02cbae'
+  license 'MIT'
 
   depends_on :macos
 
   def install
-    man1.install "man/man1/cutler.1"
-    bin.install "bin/cutler"
-    generate_completions_from_executable(bin/"cutler", "completion")
+    man1.install 'man/man1/cutler.1'
+    bin.install 'bin/cutler'
+    generate_completions_from_executable(bin / 'cutler', 'completion')
   end
 end

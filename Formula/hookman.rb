ac85356841b2e921f10579322b85983bbfcd245d
HitBlast <hitblastlive@gmail.com>
2025-05-22 00:22:27 +0600

    updated hookman

--- Formula/hookman.rb
+++ Formula/hookman.rb
@@ -2,11 +2,11 @@
 
 # Homebrew formula for Hookman: Add hooks to your Git repository using a TOML file.
 class Hookman < Formula
-  version '0.1.1'
+  version '0.1.2'
   desc 'Add hooks to your Git repository using a TOML file.'
   homepage 'https://github.com/hitblast/hookman'
   url "https://github.com/hitblast/hookman/releases/download/v#{version}/hookman-v#{version}-darwin-arm64.zip"
-  sha256 '867565e7f016f1cb22242e6173b6f6cfe939fd0468f0c9ce6dc2b5c8ebc13b29'
+  sha256 '7dab409a1de34ec321e85938d64bb04599f09f801316fc78e459b0f4edabbdb7'
   license 'MIT'
 
   depends_on :macos

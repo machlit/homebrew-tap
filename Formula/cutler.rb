3ce9b7a85fcc7dd0fae5c785e586fe38db6b80dd
HitBlast <hitblastlive@gmail.com>
2025-07-05 01:08:38 +0600

    updated version

--- Formula/cutler.rb
+++ Formula/cutler.rb
@@ -2,18 +2,18 @@
 
 # Homebrew formula for Cutler: Powerful, declarative settings management for your Mac, with speed.
 class Cutler < Formula
-  version '0.8.13'
+  version '0.8.15'
   desc 'Powerful, declarative settings management for your Mac, with speed.'
-  homepage 'https://github.com/hitblast/cutler'
+  homepage 'https://github.com/cutlercli/cutler'
 
   on_arm do
-    url "https://github.com/hitblast/cutler/releases/download/v#{version}/cutler-v#{version}-darwin-arm64.zip"
-    sha256 'b0a25ba0e898c4f5391ed4f04362dfc8d5bb0186fc5021193ff92c7b8f85a8af'
+    url "https://github.com/cutlercli/cutler/releases/download/v#{version}/cutler-v#{version}-darwin-arm64.zip"
+    sha256 '833a9b11f21e9e11a0dc1440b5064e22e4bb13007537fdf11dbb7fb460a8680f'
   end
 
   on_intel do
-    url "https://github.com/hitblast/cutler/releases/download/v#{version}/cutler-v#{version}-darwin-x86_64.zip"
-    sha256 '176aa7140e90a11ba9d5267cf20228207564f0f933f0c319134298e4e0db8ff0'
+    url "https://github.com/cutlercli/cutler/releases/download/v#{version}/cutler-v#{version}-darwin-x86_64.zip"
+    sha256 '9a4d4bc75e1a4f3c72a4f41ae96567e8080eea4d3a1a6100a4ecaf2fe4ee4f31'
   end
 
   license 'MIT'

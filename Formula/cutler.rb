e1d9c3b70306a433e137c1b5e82e870bc5cbc8a3
Shiddhartha Ashie <hitblastlive@gmail.com>
2025-07-15 22:44:57 +0600

    updated cutler formula

--- Formula/cutler.rb
+++ Formula/cutler.rb
@@ -2,18 +2,18 @@
 
 # Homebrew formula for Cutler: Powerful, declarative settings management for your Mac, with speed.
 class Cutler < Formula
-  version '0.9.5'
+  version '0.9.6'
   desc 'Powerful, declarative settings management for your Mac, with speed.'
   homepage 'https://github.com/cutlercli/cutler'
 
   on_arm do
     url "https://github.com/cutlercli/cutler/releases/download/v#{version}/cutler-v#{version}-darwin-arm64.zip"
-    sha256 '8bfbc10f868b0124c991ecb728b6e999adece5ea52d7d7a56da50ed70de8f183'
+    sha256 '80655a63fe9777f027faf860a63b7a3f3548f2fc1632f49191a64ff5f999726c'
   end
 
   on_intel do
     url "https://github.com/cutlercli/cutler/releases/download/v#{version}/cutler-v#{version}-darwin-x86_64.zip"
-    sha256 '8c5a9179188e53628164021429d91b219cf496c4afef03d800801823b7ffa1bd'
+    sha256 '43df07fd7e4816feeef7d6a2bda340cb0e13a92bea500b7b80b7bfdf17733732'
   end
 
   license 'MIT'

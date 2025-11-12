f7bc6fbf168c5b3b62d2010041de44b263025188
Anindya Shiddhartha <hitblastlive@gmail.com>
2025-11-12 23:47:55 +0600

    update cutler to v0.15.2 and drs to v0.5.2

--- Formula/cutler.rb
+++ Formula/cutler.rb
@@ -2,13 +2,13 @@
 
 # Homebrew formula for cutler: Powerful, declarative settings management for your Mac, with speed.
 class Cutler < Formula
-  version '0.15.0'
+  version '0.15.2'
   desc 'Powerful, declarative settings management for your Mac, with speed.'
   homepage 'https://github.com/machlit/cutler'
 
   if Hardware::CPU.arm?
     url "https://github.com/machlit/cutler/releases/download/v#{version}/cutler-aarch64-apple-darwin.tar.gz"
-    sha256 '4f165b1d327dbf5ef3e0d95a6443ba833af4e1c3929a038eefd77ea3a3667023'
+    sha256 '12e042467511585eb3d55312545feb859ca1d94385f25c39411c120b13a3418d'
   else
     odie "cutler is only available for macOS ARM (Apple Silicon)."
   end
--- Formula/drs.rb
+++ Formula/drs.rb
@@ -2,13 +2,13 @@
 
 # Homebrew formula for defaults-rs: Drop-in replacement for the `defaults` tool on macOS.
 class Drs < Formula
-  version '0.5.1'
+  version '0.5.2'
   desc 'Drop-in replacement for the `defaults` tool on macOS.'
   homepage 'https://github.com/machlit/defaults-rs'
 
   if Hardware::CPU.arm?
     url "https://github.com/machlit/defaults-rs/releases/download/v#{version}/drs-aarch64-apple-darwin.zip"
-    sha256 '14fa9fc8f58af2f9063984dbb00c31683c3a95766c8c2ec71f36a6bb7d7abc33'
+    sha256 'b42dec8f692664ac107fe4d9fce40affd751bbdbcacb2844e6e6323f6eb6b52e'
   else
     odie "drs is only available for macOS ARM (Apple Silicon)."
   end

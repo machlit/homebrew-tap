8ff395664f800e839e40830b6af4e5bd092ab70b
Anindya Shiddhartha <hitblastlive@gmail.com>
2025-11-12 10:56:19 +0600

    chore: update drs

--- Formula/drs.rb
+++ Formula/drs.rb
@@ -2,13 +2,13 @@
 
 # Homebrew formula for defaults-rs: Drop-in replacement for the `defaults` tool on macOS.
 class Drs < Formula
-  version '0.4.1'
+  version '0.5.1'
   desc 'Drop-in replacement for the `defaults` tool on macOS.'
   homepage 'https://github.com/hitblast/defaults-rs'
 
   if Hardware::CPU.arm?
     url "https://github.com/hitblast/defaults-rs/releases/download/v#{version}/drs-aarch64-apple-darwin.zip"
-    sha256 'eb6c79fdeea99bbaf13e8c74133524822bf8e427b13633a983ea8263450e606a'
+    sha256 '14fa9fc8f58af2f9063984dbb00c31683c3a95766c8c2ec71f36a6bb7d7abc33'
   else
     odie "drs is only available for macOS ARM (Apple Silicon)."
   end

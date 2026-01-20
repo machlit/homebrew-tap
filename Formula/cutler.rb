510f9141e53f53c1d48b1442f35b36e90bb08725
Anindya Shiddhartha <shiddhartha@tuta.io>
2026-01-20 18:24:55 +0600

    update cutler to v0.19.6

--- Formula/cutler.rb
+++ Formula/cutler.rb
@@ -2,13 +2,13 @@
 
 # Homebrew formula for cutler: Powerful, declarative settings management for your Mac, with speed.
 class Cutler < Formula
-  version '0.19.5'
+  version '0.19.6'
   desc 'Powerful, declarative settings management for your Mac, with speed.'
   homepage 'https://github.com/machlit/cutler'
 
   if Hardware::CPU.arm?
     url "https://github.com/machlit/cutler/releases/download/v#{version}/cutler-aarch64-apple-darwin.tar.gz"
-    sha256 '58e631957cc7723931453d7f202fcb5a4c72bfd66d393d0f8e93733b7dd80c04'
+    sha256 'd07e7187d0331809084096d3e1c9192aa74003be07963c00f4b45bd38067671b'
   else
     odie "cutler is only available for macOS ARM (Apple Silicon)."
   end
